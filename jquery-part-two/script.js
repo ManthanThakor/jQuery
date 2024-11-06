8f2f2e497dc945ac72a5f12032f47b92e6816f4b
ManthanThakor <thakormanthan849@gmail.com>
2024-11-06 10:34:35 +0530

    done

--- jquery-part-two/script.js
+++ jquery-part-two/script.js
@@ -1,77 +1,18 @@
 $(document).ready(function () {
-  // Get Children
-  $(".get-children").click(function () {
-    const children = $(this).siblings(".parent").children(".child");
-    alert(
-      "Children: " +
-        children
-          .map(function () {
-            return $(this).text();
-          })
-          .get()
-          .join(", ")
-    );
+  // Ancestors example
+  $(".child").click(function () {
+    $(this).parent().css("background-color", "yellow");
+    $(this).parents(".ancestor").css("border", "2px solid red");
   });
 
-  // Get Siblings
-  $(".get-siblings").click(function () {
-    const siblings = $(this)
-      .siblings(".sibling-container")
-      .children(".sibling");
-    alert(
-      "Siblings: " +
-        siblings
-          .map(function () {
-            return $(this).text();
-          })
-          .get()
-          .join(", ")
-    );
-  });
+  // Descendants example
+  $(".container").find("p").css("color", "blue");
 
-  // Find Element
-  $(".find-element").click(function () {
-    const foundElement = $(this).siblings(".finder").find(".find-me");
-    alert("Found Element: " + foundElement.text());
-  });
+  // Siblings example
+  $(".second").siblings().css("font-weight", "bold");
+  $(".first").next().css("color", "green");
 
-  // Closest Traversing
-  $(".closest-button").click(function () {
-    const closestOuter = $(this).closest(".outer");
-    alert("Closest Outer Class: " + closestOuter.attr("class"));
-  });
-
-  // Get Descendants
-  $(".get-descendants").click(function () {
-    const descendants = $(this).siblings(".ancestor").find(".descendant");
-    alert(
-      "Descendants: " +
-        descendants
-          .map(function () {
-            return $(this).text();
-          })
-          .get()
-          .join(", ")
-    );
-  });
-
-  // Get Ancestor
-  $(".get-ancestor").click(function () {
-    const ancestor = $(this).closest(".card").find(".ancestor");
-    alert("Ancestor Class: " + ancestor.attr("class"));
-  });
-
-  // Filter Fruits
-  $(".filter-fruits").click(function () {
-    const fruits = $(".filter-item").filter("[data-category='fruit']");
-    alert(
-      "Fruits: " +
-        fruits
-          .map(function () {
-            return $(this).text();
-          })
-          .get()
-          .join(", ")
-    );
-  });
+  // Filtering example
+  $(".items li").filter(":even").css("font-weight", "bold");
+  $(".items li").not(":eq(2)").css("color", "red");
 });
